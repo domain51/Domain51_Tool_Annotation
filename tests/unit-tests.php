2954b632210bc6d17da718af01e5baf07589a2cf
tswicegood <tswicegood@159b4bd8-5ae1-4aa7-97ff-061b2ff7a14b>
2007-06-26 22:50:24 +0000

    Bring the unit-tests file and make it work with just this package
    git-svn-id: svn://localhost/Domain51_Tool_Annotation/trunk@108 159b4bd8-5ae1-4aa7-97ff-061b2ff7a14b

---
+++ tests/unit-tests.php
@@ -0,0 +1,13 @@
+<?php
+
+require_once dirname(__FILE__) . '/bootstrap.php';
+
+class Domain51_Tool_AnnotationTestSuite extends TestSuite
+{
+    public function __construct() 
+    {
+        $this->addTestFile('Domain51/Tool/Annotation/CollectionTest.php');
+        $this->addTestFile('Domain51/Tool/Annotation/ParserTest.php');
+        $this->addTestFile('Domain51/Tool/Annotation/ValueTest.php');
+    }
+}
