3f21b0f3a69f7a04b7269f01c129ec91c986b78f
tswicegood <tswicegood@159b4bd8-5ae1-4aa7-97ff-061b2ff7a14b>
2007-06-29 22:54:36 +0000

    Add new param helper tests to test suite
    git-svn-id: svn://localhost/Domain51_Tool_Annotation/trunk@128 159b4bd8-5ae1-4aa7-97ff-061b2ff7a14b

--- tests/unit-tests.php
+++ tests/unit-tests.php
@@ -7,6 +7,8 @@ class Domain51_Tool_AnnotationTestSuite extends TestSuite
     public function __construct() 
     {
         $this->addTestFile('Domain51/Tool/Annotation/CollectionTest.php');
+        $this->addTestFile('Domain51/Tool/Annotation/ParamsHelper/AsUMLMethodArgumentsTest.php');
+        $this->addTestFile('Domain51/Tool/Annotation/ParamsHelper/ExplodeOnWhitespaceTest.php');
         $this->addTestFile('Domain51/Tool/Annotation/ParserTest.php');
         $this->addTestFile('Domain51/Tool/Annotation/ValueTest.php');
     }
